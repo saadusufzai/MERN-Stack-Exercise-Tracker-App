74f68960a580135001ef24380409e618c9c3f833
Saad Aslam <saadusufzai@gmail.com>
2020-10-10 20:16:45 +0500

    routes added

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const exerciseRouter = require('./routes/exercise');
+const userRouter = require('./routes/users');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', userRouter)
+
 app.listen(port, () => {
   console.log(`Server is runnuing on port: ${port}`);
   const uri = process.env.ATLAS_URI;
