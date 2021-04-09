97029ecac7ec026ea0c9c81c11dcfd5e56ab1fdc
Saad Aslam <saadusufzai@gmail.com>
2021-04-09 18:35:34 +0500

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
 let User = require("../models/user.model");
 
+/**
+ * @route   GET users/
+ * @desc    Get All Users
+ * @access  Pulic
+ */
+
 router.route("/").get((req, res) => {
   User.find()
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json("Error:" + err));
 });
+
+/**
+ * @route   GET users/add
+ * @desc    Add new User
+ * @access  Public
+ */
 
 router.route("/add").post((req, res) => {
   const username = req.body.username;
@@ -17,10 +29,32 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+
+
+/**
+ * @route   GET users/id
+ * @desc    Get Single User by id
+ * @access  Public
+ */
+
+router.route('/:id').get((req, res)=>{
+  User.findById(req.params.id)
+  .then((user)=> res.json(user))
+  .catch((err) => res.status(400).json("Error: " + err))
+}) 
+
+
+
+/**
+ * @route   DELETE users/id
+ * @desc    Delete User by id
+ * @access  Public
+ */
+
 router.route('/:id').delete((req, res) => {
-  const id = req.params.id;
-  User.findByIdAndDelete(id)
-  .then((users) => res.json({msg: "User deleted!"}))
+
+  User.findByIdAndDelete(req.params.id)
+  .then(() => res.json({msg: "User deleted!"}))
   .catch((err) => res.status(400).json("Error: " + err));
 })
 
