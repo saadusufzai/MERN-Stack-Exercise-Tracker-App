74aecfc7665aef945c98c0fde13391c64ceec6d7
Saad Aslam <saadusufzai@gmail.com>
2021-04-09 19:15:29 +0500

    Update Users.jsx

--- frontend/src/components/user/Users.jsx
+++ frontend/src/components/user/Users.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 const Users = () => {
     return (
         <div>
-            
+             <h3>Logged Exercises</h3>
+    <table className="table">
+      <thead className="thead-light">
+        <tr>
+          <th>Username</th>
+          <th>Description</th>
+          <th>Duration</th>
+          <th>Date</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* { <ExerciseList/> } */}
+      </tbody>
+    </table> 
         </div>
     )
 }
