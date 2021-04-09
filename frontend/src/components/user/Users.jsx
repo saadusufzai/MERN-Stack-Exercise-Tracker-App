52170ef9d6c97ec98b86e5748b86a35a6a99fd09
Saad Aslam <saadusufzai@gmail.com>
2021-04-09 19:49:38 +0500

    Update Users.jsx

--- frontend/src/components/user/Users.jsx
+++ frontend/src/components/user/Users.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect,useState }from 'react'
+import axios from 'axios'
 
 const Users = () => {
+
+  const [username, setUsername] = useState()
+
+  useEffect(() => {
+    const config = {
+    
+    header : {
+      "Access-Control-Allow-Origin" : "",
+      "Allow": "GET",
+      "Content-type": "Application/json",
+    }
+  }
+    axios.get('localhost:5000/users/',config)
+    .then((res)=>{
+      console.log(res)
+      setUsername(res)
+
+    })
+    .catch(err=>console.error(err))
+
+  },[])
+
+
+
     return (
         <div>
              <h3>Logged Exercises</h3>
@@ -8,9 +33,7 @@ const Users = () => {
       <thead className="thead-light">
         <tr>
           <th>Username</th>
-          <th>Description</th>
-          <th>Duration</th>
-          <th>Date</th>
+          <th>Registration Date</th>
           <th>Actions</th>
         </tr>
       </thead>
