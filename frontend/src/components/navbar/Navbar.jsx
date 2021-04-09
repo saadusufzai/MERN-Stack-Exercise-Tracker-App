d37a17e31b9ebfdbec704fad437a959126439233
Saad Aslam <saadusufzai@gmail.com>
2021-04-09 21:10:14 +0500

    updated responsive menu

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-            <Link to='/' className='navbar-brand'>ExerciseTracker</Link>
-            <div className="collapse navbar-collapse">
-                <ul className="navbar-nav mr-auto">
-                    <li className="navbar-item mr-2">
-                        <Link to='/' className="nav-link">Exercises</Link>
-                    </li>
-                    <li className="navbar-item mr-2">
-                        <Link to='/create' className="nav-link">Create Exercise Log</Link>
-                    </li>
-                    <li className="navbar-item mr-2">
-                        <Link to='/users' className="nav-link">Users</Link>
-                    </li>
-                    <li className="navbar-item mr-2">
-                        <Link to='/create-user' className="nav-link">Create User</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <Link to="/" className="navbar-brand">
+        ExerciseTracker
+      </Link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="navbar-item mr-2">
+            <Link to="/" className="nav-link">
+              Exercises
+            </Link>
+          </li>
+          <li className="navbar-item mr-2">
+            <Link to="/create" className="nav-link">
+              Create Exercise Log
+            </Link>
+          </li>
+          <li className="navbar-item mr-2">
+            <Link to="/users" className="nav-link">
+              Users
+            </Link>
+          </li>
+          <li className="navbar-item mr-2">
+            <Link to="/create-user" className="nav-link">
+              Create User
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
