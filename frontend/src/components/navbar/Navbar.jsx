93a34eaebfd815b3c002a6c45849a19a299ff5ca
Saad Aslam <saadusufzai@gmail.com>
2021-04-09 18:42:54 +0500

    Update Navbar.jsx

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -7,14 +7,17 @@ const Navbar = () => {
             <Link to='/' className='navbar-brand'>ExerciseTracker</Link>
             <div className="collapse navbar-collapse">
                 <ul className="navbar-nav mr-auto">
-                    <li className="navbar-item">
+                    <li className="navbar-item mr-2">
                         <Link to='/' className="nav-link">Exercises</Link>
                     </li>
-                    <li className="navbar-item">
+                    <li className="navbar-item mr-2">
                         <Link to='/create' className="nav-link">Create Exercise Log</Link>
                     </li>
-                    <li className="navbar-item">
-                        <Link to='/user' className="nav-link">Create User</Link>
+                    <li className="navbar-item mr-2">
+                        <Link to='/users' className="nav-link">Users</Link>
+                    </li>
+                    <li className="navbar-item mr-2">
+                        <Link to='/create-user' className="nav-link">Create User</Link>
                     </li>
                 </ul>
             </div>
